cf739db7e2ffad07a30772993aed07bd47f7939a
Harry-Potter415 <olivedreamdev@gmail.com>
2021-04-12 02:19:57 +0900

    tenth commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Board extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      squares = Array(9).fill(null),
+      squares : Array(9).fill(null),
     };
   }
 
