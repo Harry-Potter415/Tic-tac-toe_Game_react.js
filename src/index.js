9fd9a74c9bdf9b627dadb6c53f2868d1608f82e6
Harry-Potter415 <olivedreamdev@gmail.com>
2021-04-12 02:29:08 +0900

    eleventh commit

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={()=>this.setState({value:'X'})}>
-        {this.state.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
@@ -29,8 +29,15 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]}
+    onClick = {() => this.handleClick(i)} />;
   }
 
   render() {
