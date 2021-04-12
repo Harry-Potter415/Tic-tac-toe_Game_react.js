e9ee628f2da224b413bb45fe3c2dc0227f8de552
Harry-Potter415 <olivedreamdev@gmail.com>
2021-04-13 08:40:51 +0900

    seventeenth commit

--- src/index.js
+++ src/index.js
@@ -76,6 +76,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li>
+          <button onClick ={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -93,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
